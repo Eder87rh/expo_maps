d5de90e7d2e71287fbeef906027d495574ec9a0e
Eder <eder.ramirez87@gmail.com>
2017-08-12 22:57:07 -0500

    Tracking position in realtime, and button for get Places

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Platform, Text, View, StyleSheet, Dimensions } from 'react-native';
+import { Platform, Text, View, StyleSheet, Dimensions, TouchableHighlight } from 'react-native';
 import { Constants, Location, Permissions, MapView } from 'expo';
 
 /**
  *https://maps.googleapis.com/maps/api/place/nearbysearch/json?
  *location=-33.8670522,151.1957362&radius=500&type=food&key=YOUR_API_KEY
 **/
-const {width,heiht} = Dimensions.get('window')
+const { width, heiht } = Dimensions.get('window')
+const LATITUDEDELTA = 0.0922;
+const LONGITUDEDELTA = 0.922;
 
 export default class App extends Component {
   constructor() {
@@ -19,7 +21,7 @@ export default class App extends Component {
         latitudeDelta: null,
         longitudeDelta: null,
       },
-      places:null,
+      places: null,
       errorMessage: null,
     };
   }
@@ -34,17 +36,17 @@ export default class App extends Component {
     }
   }
 
-  getUrlParameters(lat,lon,radius, type,API){
+  getUrlParameters(lat, lon, radius, type, API) {
     const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
     const location = `location=${lat},${lon}&radius=${radius}`
     const typeData = `&type=${type}`
     const key = `&key=${API}`
 
-    return url+location+typeData+key
+    return url + location + typeData + key
   }
 
-  getPlaces(lat,lon){
-    const url = this.getUrlParameters(lat,lon,1000,'food','AIzaSyC-EpAyfsk1465-CUMWvTzAv9uSRsW9o0M');
+  getPlaces() {
+    const url = this.getUrlParameters(this.state.region.latitude, this.state.region.longitude, 1000, 'food', 'AIzaSyC-EpAyfsk1465-CUMWvTzAv9uSRsW9o0M');
     fetch(url)
       .then((data) => data.json())
       .then((data) => {
@@ -54,26 +56,26 @@ export default class App extends Component {
           console.log(element)
           arrayMarkers.push(
             <MapView.Marker
-              key = {i}
-              coordinate = {{
-                latitude : element.geometry.location.lat,
-                longitude : element.geometry.location.lng
+              key={i}
+              coordinate={{
+                latitude: element.geometry.location.lat,
+                longitude: element.geometry.location.lng
               }}
             >
               <MapView.Callout>
                 <View>
                   <Text>{element.name}</Text>
-                   <Text>{element.opening_hours? 'ABIERTO AHORA': 'CERRADO'}</Text> 
+                  <Text>{element.opening_hours ? 'ABIERTO AHORA' : 'CERRADO'}</Text>
                 </View>
               </MapView.Callout>
             </MapView.Marker>
           )
         })
         this.setState({
-          places : arrayMarkers
+          places: arrayMarkers
         })
       })
-    
+
   }
 
   _getLocationAsync = async () => {
@@ -84,52 +86,66 @@ export default class App extends Component {
       });
     }
 
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ location });
-
-    const lat = location.coords.latitude
-    const lon = location.coords.longitude
-    const accuracy = location.coords.accuracy
-    this.calcDelta(lat, lon, accuracy)
-
-    this.getPlaces(lat,lon)
-  };
-
-  calcDelta(lat, lon, accuracy) {
-    const oneDegreeOfLongitudeInMeters = 111.32;
-    const circunference = (40075 / 360)
-
-    const latDelta = accuracy * (1 / (Math.cos(lat) * circunference))
-    const lonDelta = (accuracy / oneDegreeOfLongitudeInMeters)
-
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true
+    });
     this.setState({
       region: {
-        latitude: lat,
-        longitude: lon,
-        latitudeDelta: latDelta,
-        longitudeDelta: lonDelta,
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: LATITUDEDELTA,
+        longitudeDelta: LONGITUDEDELTA
       }
     })
+
+    await Location.watchPositionAsync(
+      {
+        enableHighAccuracy: true,
+        imeInterval: 20000
+      },
+      (position) => {
+        const newRegion = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: LATITUDEDELTA,
+          longitudeDelta: LONGITUDEDELTA
+        }
+        this.setState({ region: newRegion })
+      }
+    )
+  };
+
+
+  showPlaces = () => {
+    this.setState({ places: null })
+    this.getPlaces();
   }
+
+
 
   render() {
 
     return (
       <View style={styles.container}>
+        <TouchableHighlight
+          style={{
+            position: 'absolute',
+            bottom: 0,
+            right: 0,
+            zIndex: 2
+          }}
+          onPress={() => this.showPlaces()}>
+          <Text>Show Food Places</Text>
+        </TouchableHighlight>
         {this.state.region.latitude ?
           <MapView
             style={styles.map}
-            initialRegion={this.state.region}>
-          
-            <MapView.Marker coordinate = {{ 
-              latitude: this.state.region.latitude,
-              longitude: this.state.region.longitude
-            }}>
-              <View style={styles.marker} />
-            </MapView.Marker>
+            region={this.state.region}
+            showsUserLocation={true}
+            followsUserLocation={true}>
 
             {this.state.places}
-            
+
           </MapView>
           :
           null}
@@ -154,9 +170,9 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
-    width:width
+    width: width
   },
-   marker: {
+  marker: {
     width: 8,
     height: 8,
     borderRadius: 4,
